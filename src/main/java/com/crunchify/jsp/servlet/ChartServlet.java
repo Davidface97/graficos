331c899e47ffcd92318e817f7d53cbd11ff3587a
Labing <Labing@XeonB101-PC-18.academia.loc>
2017-06-01 10:55:27 -0500

    Todo

--- src/main/java/com/crunchify/jsp/servlet/ChartServlet.java
+++ src/main/java/com/crunchify/jsp/servlet/ChartServlet.java
@@ -7,6 +7,7 @@ package com.crunchify.jsp.servlet;
 
 import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.Paint;
 import java.io.IOException;
 import java.io.OutputStream;
 
@@ -17,41 +18,100 @@ import javax.servlet.http.HttpServletResponse;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartUtilities;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.axis.ValueAxis;
+import org.jfree.chart.labels.ItemLabelAnchor;
+import org.jfree.chart.labels.ItemLabelPosition;
+import org.jfree.chart.plot.CategoryPlot;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.renderer.category.CategoryItemRenderer;
+import org.jfree.data.category.DefaultCategoryDataset;
 import org.jfree.data.general.DefaultPieDataset;
+import org.jfree.ui.TextAnchor;
 
 public class ChartServlet extends HttpServlet {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        	response.setContentType("image/png");
-		OutputStream outputStream = response.getOutputStream();
-		JFreeChart chart = getChart();
-		int width = 500;
-		int height = 350;
-		ChartUtilities.writeChartAsPNG(outputStream, chart, width, height);
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("image/png");
+        OutputStream outputStream = response.getOutputStream();
+        JFreeChart chart = getChart();
+        JFreeChart chart2 = getChart2();
+        int width = 500;
+        int height = 350;
+        ChartUtilities.writeChartAsPNG(outputStream, chart, width, height);
+        ChartUtilities.writeChartAsPNG(outputStream, chart2, width, height);
 
-	}
+    }
 
-	public JFreeChart getChart() {
-		
-                DefaultPieDataset dataset = new DefaultPieDataset();
-	        //Crear la capa de servicios que se enlace con el DAO
-                dataset.setValue("Ford", 23.3);
-		dataset.setValue("Chevy", 32.4);
-		dataset.setValue("Yugo", 44.2);
+    public JFreeChart getChart() {
 
-		boolean legend = true;
-		boolean tooltips = false;
-		boolean urls = false;
+        DefaultPieDataset dataset = new DefaultPieDataset();
+        //Crear la capa de servicios que se enlace con el DAO
+        dataset.setValue("Ford", 23.3);
+        dataset.setValue("Chevy", 32.4);
+        dataset.setValue("Yugo", 44.2);
 
-		JFreeChart chart = ChartFactory.createPieChart("Cars", dataset, legend, tooltips, urls);
+        boolean legend = true;
+        boolean tooltips = false;
+        boolean urls = false;
 
-		chart.setBorderPaint(Color.GREEN);
-		chart.setBorderStroke(new BasicStroke(5.0f));
-		chart.setBorderVisible(true);
+        JFreeChart chart = ChartFactory.createPieChart("Cars", dataset, legend, tooltips, urls);
 
-		return chart;
-	}
+        chart.setBorderPaint(Color.GREEN);
+        chart.setBorderStroke(new BasicStroke(5.0f));
+        chart.setBorderVisible(true);
+
+        return chart;
+    }
+
+    public JFreeChart getChart2() {
+
+        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+        dataset.addValue(15, "1", "451");
+        dataset.addValue(12, "1", "851");
+        dataset.addValue(10, "2", "362");
+        dataset.addValue(5, "2", "142");
+
+        JFreeChart chart = ChartFactory.createBarChart(
+                "Kilos Por Colmena", // chart title
+                "Category", // domain axis label
+                "Value", // range axis label
+                dataset, // data
+                PlotOrientation.VERTICAL, // the plot orientation
+                false, // include legend
+                true,
+                false
+        );
+
+        chart.setBackgroundPaint(Color.lightGray);
+
+        // get a reference to the plot for further customisation...
+        CategoryPlot plot = chart.getCategoryPlot();
+        plot.setNoDataMessage("NO DATA!");
+
+        CategoryItemRenderer renderer = new CustomRenderer(
+                new Paint[]{Color.red, Color.blue, Color.green,
+                    Color.yellow, Color.orange, Color.cyan,
+                    Color.magenta, Color.blue}
+        );
+
+        renderer.setItemLabelsVisible(true);
+        ItemLabelPosition p = new ItemLabelPosition(
+                ItemLabelAnchor.CENTER, TextAnchor.CENTER, TextAnchor.CENTER, 45.0
+        );
+        renderer.setPositiveItemLabelPosition(p);
+        plot.setRenderer(renderer);
+
+        // change the margin at the top of the range axis...
+        ValueAxis rangeAxis = plot.getRangeAxis();
+        rangeAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
+        rangeAxis.setLowerMargin(0.15);
+        rangeAxis.setUpperMargin(0.15);
+
+        return chart;
+
+    }
 
 }
